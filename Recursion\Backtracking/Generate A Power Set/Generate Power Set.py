08a062bdae2a79c5618b901936529157e28a1eab
Towfiq Ahmed <128798014+TowfiqDL@users.noreply.github.com>
2023-05-03 14:39:10 +0600

    Added a new recursive approach

--- Recursion\Backtracking/Generate A Power Set/Generate Power Set.py
+++ Recursion\Backtracking/Generate A Power Set/Generate Power Set.py
@@ -17,3 +17,18 @@ def generate_power_set(elements):
   currSet, subSets = [], []
   helper(0, currSet, subsets)
   return subSets
+
+
+## Without Using Dynamic Array as stack. For example: to find the power set of [1, 2], we find the set without [1] and with [1] and find the intersection of these two sets. 
+
+def generate_power_set(elements): 
+  def helper(indx, selected_sets):
+    if indx >= len(elements):
+      power_set.append(selected_sets)
+      return 
+    helper(indx+1, selected_sets) 
+    helper(indx+1, selected_sets + [elements[indx])
+  
+  power_set = []
+  helper(0, []) 
+  return power_set
